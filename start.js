898694f9ee180c7b9c437cdeb9d6d84d9d67fc30
Bruno Luis <gen.brunoluis@gmail.com>
2021-08-01 11:54:15 -0300

    Bloqueando o Resizable

--- start.js
+++ start.js
@@ -3,8 +3,15 @@ const { app, BrowserWindow } = require('electron');
 function createWindow () {
     const win = new BrowserWindow({
       width: 800,
-      height: 600
+      height: 600,
+      resizable: false,
+      webPreferences: {
+        nodeIntegration: true,
+        nodeIntegrationInWorker: true,
+        contextIsolation: false
+      }
     })
+    
   
     win.loadFile('./www/pages/index.html')
 }
